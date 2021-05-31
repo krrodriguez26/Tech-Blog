7330c3835a846a96a76dd122dae20dc4ac4e896d
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-31 16:00:19 -0400

    Troubleshooting routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('home-routes.js');
+const dashboardRoutes = require('dashboard-routes.js');
 
 const apiRoutes = require('./api');
 
